5a7fd0e9989234bbb7b531717f96e621855e0dd7
Eloy José Armao Mosquera <124069891+EloyArmaojr90@users.noreply.github.com>
2023-11-02 15:28:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ async def get_actor(plataforma: str, año: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
